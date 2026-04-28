3a771ad9ce4239b907931a415e44c643c68e2b05
flexsana0019 <starrikovaalina@gmail.com>
2026-04-28 18:55:06 +0700

    add: aboutSection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header/Header";
 import WelcomeSection from "./components/WelcomeSection/WelcomeSection";
+import AboutSection from "./components/AboutSection/AboutSection";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Header />
       <main>
         <WelcomeSection />
+        <AboutSection />
       </main>
     </div>
   );
