7fe705191ce2f46f328409bca0b132d60ab4ead9
flexsana0019 <starrikovaalina@gmail.com>
2026-04-28 22:28:50 +0700

    add: featuresSection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
 import WelcomeSection from "./components/WelcomeSection/WelcomeSection";
 import AboutSection from "./components/AboutSection/AboutSection";
+import FeaturesSection from "./components/FeaturesSection/FeaturesSection";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <main>
         <WelcomeSection />
         <AboutSection />
+        <FeaturesSection />
       </main>
     </div>
   );
