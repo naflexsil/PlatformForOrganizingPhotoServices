c7a1071645a91221f3aceb7b84ea361c5137a3ec
flexsana0019 <starrikovaalina@gmail.com>
2026-04-28 18:16:04 +0700

    edit: visual welcomeSection + header

--- frontend/src/components/WelcomeSection/WelcomeSection.jsx
+++ frontend/src/components/WelcomeSection/WelcomeSection.jsx
@@ -5,17 +5,27 @@ import welcomeImg from "../../assets/images/picWelcomeSection.png";
 const WelcomeSection = () => {
   return (
     <section className={s.welcome}>
-      <div className={s.textSide}>
-        <h1 className={s.title}>
-          МОМЕНТЫ, <br />
-          <span className={s.highlight}>КОТОРЫЕ СТОИТ ПОМНИТЬ</span>
-        </h1>
-        <p className={s.subtitle}>ФОТОГРАФ, КОТОРЫЙ ИХ ПОЧУВСТВУЕТ</p>
-        <button className={s.actionBtn}>Найти фотографа</button>
-      </div>
+      <div className={s.contentWrapper}>
+        <div className={s.textSide}>
+          <h1 className={s.title}>
+            <span className={s.pink}>МОМЕНТЫ</span>
+            <span className={s.red}>,</span> <br />
+            <span className={s.red}>КОТОРЫЕ СТОИТ</span>{" "}
+            <span className={s.pink}>ПОМНИТЬ</span>
+          </h1>
 
-      <div className={s.imageSide}>
-        <img src={welcomeImg} alt="Коллаж работ" className={s.mainImage} />
+          <h2 className={`${s.title} ${s.subtitle}`}>
+            <span className={s.pink}>ФОТОГРАФ</span>
+            <span className={s.red}>,</span>{" "}
+            <span className={s.red}>КОТОРЫЙ ИХ</span> <br />
+            <span className={s.pink}>ПОЧУВСТВУЕТ</span>
+          </h2>
+        </div>
+
+        <div className={s.imageSide}>
+          <img src={welcomeImg} alt="Коллаж работ" className={s.mainImage} />
+          <button className={s.actionBtn}>Найти фотографа</button>
+        </div>
       </div>
     </section>
   );
