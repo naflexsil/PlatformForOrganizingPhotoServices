b416f38f1263d1414533d063a5df3560d6a8d884
flexsana0019 <starrikovaalina@gmail.com>
2026-04-30 21:07:51 +0700

    add: feedSection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header/Header";
 import WelcomeSection from "./components/WelcomeSection/WelcomeSection";
 import AboutSection from "./components/AboutSection/AboutSection";
 import FeaturesSection from "./components/FeaturesSection/FeaturesSection";
+import FeedSection from "./components/FeedSection/FeedSection";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <WelcomeSection />
         <AboutSection />
         <FeaturesSection />
+        <FeedSection />
       </main>
     </div>
   );
---
+++ frontend/src/components/FeedSection/FeedSection.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import s from "./FeedSection.module.css";
+import feedPhoto from "../../assets/images/feed_preview.png";
+
+const FeedSection = () => {
+  return (
+    <section className={s.feed}>
+      <div className={s.container}>
+        <div className={s.mainCard}>
+          <div className={s.textContent}>
+            <h2 className={s.title}>ЛЕНТА ВДОХНОВЕНИЯ</h2>
+            <p className={s.description}>
+              Не знаете, какой образ себе подобрать? <br />
+              Какой ракурс будет тем самым? <br />
+              Или может в какую позу лучше встать? <br />
+              Посмотрите, что фотографируют другие!
+            </p>
+            <button className={s.showBtn}>Перейти на ленту</button>
+          </div>
+
+          <div className={s.imageWrapper}>
+            <img src={feedPhoto} alt="Лента вдохновения" className={s.photo} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FeedSection;
